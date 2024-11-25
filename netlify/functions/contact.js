afa939b1498a19da90bf4aae8013872496abee20
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2024-11-25 15:53:31 +0100

    chore

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -1,4 +1,4 @@
-import { createTransport } from "nodemailer";
+import { Resend } from "resend";
 
 export async function handler(event) {
   if (event.httpMethod !== "POST") {
@@ -10,35 +10,13 @@ export async function handler(event) {
 
   const { name, email, message } = JSON.parse(event.body);
 
-  // Configure the Nodemailer transporter for Outlook
-  const transporter = createTransport({
-    host: "smtp.office365.com", // Outlook SMTP server
-    port: 587, // Port for TLS
-    secure: false, // Use TLS (not SSL)
-    auth: {
-      user: process.env.REACT_APP_OUTLOOK, // Use your Outlook email
-      pass: process.env.REACT_APP_OUTLOOK_PASS, // Use your Outlook password
-    },
+  const resend = new Resend("re_PcxNN4qT_8AhGDUG4jj1noW186msVjNbF");
+
+  resend.emails.send({
+    from: "goksel_ozkazanc_08@hotmail.com",
+    to: "armatechemail@gmail.com",
+    subject: "Hello World",
+    html: "<p>Congrats on sending your <strong>first email</strong>!</p>",
   });
 
-  const mailOptions = {
-    from: `"${name}" <${email}>`, // Sender details
-    to: process.env.REACT_APP_OUTLOOK, // Your Outlook email address
-    subject: "New Contact Form Submission",
-    text: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
-  };
-
-  try {
-    await transporter.sendMail(mailOptions);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: "Email sent successfully!" }),
-    };
-  } catch (error) {
-    console.error("Error sending email:", error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Failed to send email." }),
-    };
-  }
 }
