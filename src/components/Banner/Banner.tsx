01d679b4e335fc4e4e1bac12720a8257d86a3508
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2024-11-25 02:10:01 +0100

    fix video screen

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,8 +1,8 @@
 import styled from "@mui/system/styled";
 import { keyframes } from "@emotion/react";
-import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import Logo from "../../assets/images/armatech-logo-opac.png";
 
-// Define the fade-in animation using keyframes
 const fadeIn = keyframes`
   from {
     opacity: 0;
@@ -12,59 +12,51 @@ const fadeIn = keyframes`
   }
 `;
 
-// Styled Components
 const BannerContainer = styled("div")({
   position: "relative",
   width: "100%",
   height: "100vh",
   overflow: "hidden",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
 });
 
 const StyledIframe = styled("iframe")({
   width: "100%",
-  height: "100%",
+  aspectRatio: "16 / 9",
   border: "none",
   objectFit: "cover",
 });
 
-const Overlay = styled("div")({
-  position: "absolute",
-  top: 0,
-  left: 0,
-  width: "100%",
-  height: "100%",
-  backgroundColor: "rgba(0, 0, 0, 0.5)",
-  zIndex: 1,
-});
-
-const BannerText = styled(Typography)(({ theme }) => ({
+const BannerLogo = styled(Box)(({ theme }) => ({
+  width: "120px",
   position: "absolute",
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  color: "#b4b4b4",
-  fontWeight: "bold",
-  textAlign: "center",
   zIndex: 2,
-  textShadow: "0px 9px 10px rgba(0, 0, 0, 0.7)",
-  animation: `${fadeIn} 1.5s ease-in-out`, // Use the keyframes animation
+  animation: `${fadeIn} 1.5s ease-in-out`,
+  [theme.breakpoints.down("md")]: {
+    width: "80px",
+  },
   [theme.breakpoints.down("sm")]: {
-    fontSize: "0.5rem",
+    width: "60px",
   },
 }));
 
-// Main Component
 const Banner = () => {
   return (
     <BannerContainer>
       <StyledIframe
-        src="https://www.youtube.com/embed/OS_UW8W7oJ4?autoplay=1&mute=1&loop=1&playlist=OS_UW8W7oJ4&controls=0&rel=0"
+        src="https://www.youtube.com/embed/O9aKS1BW95s?autoplay=1&mute=1&loop=1&playlist=O9aKS1BW95s&controls=0&rel=0"
         allow="autoplay; encrypted-media"
         allowFullScreen
         title="Banner Video"
       />
-      <Overlay />
-      <BannerText variant="h6">ARMATECH CNC Çözümleri</BannerText>
+      <BannerLogo id="haha" sx={{ cursor: "pointer" }}>
+        <img src={Logo} width="100%" />
+      </BannerLogo>
     </BannerContainer>
   );
 };
