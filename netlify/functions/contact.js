7d7efb1e3bf04911e1ebef08f51a218c72409032
Goksel Ozkazanc <goksel_ozkazanc_08@hotmail.com>
2024-11-26 14:05:56 +0100

    chore

--- netlify/functions/contact.js
+++ netlify/functions/contact.js
@@ -18,27 +18,32 @@ export async function handler(event) {
 
   console.log("Sending email...");
 
-  await resend.emails
-    .send({
+  try {
+    const response = await resend.emails.send({
       from: "no-reply@resend.dev",
       to: "armatech31@gmail.com",
       reply_to: email,
       subject: `New message from ${name}`,
       html: `
-      <p><strong>Name:</strong> ${name}</p>
-      <p><strong>Email:</strong> ${email}</p>
-      <p><strong>Message:</strong></p>
-      <p>${message}</p>
-    `,
-    })
-    .then((response) => {
-      console.log("Email sent!", response);
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Email sent!" }),
-      };
-    })
-    .catch((error) => {
-      console.error("Error sending email", error);
+        <p><strong>Name:</strong> ${name}</p>
+        <p><strong>Email:</strong> ${email}</p>
+        <p><strong>Message:</strong></p>
+        <p>${message}</p>
+      `,
     });
+
+    console.log("Email sent successfully:", response);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully!" }),
+    };
+  } catch (error) {
+    console.error("Error sending email:", error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Failed to send email.", error }),
+    };
+  }
 }
